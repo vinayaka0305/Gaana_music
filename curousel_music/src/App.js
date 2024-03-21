06363ced3be762bc1ed82c9a42410d61ce5b24b3
vinayaka0305 <vav6473@gmail.com>
2024-03-21 20:24:55 +0530

    code

--- curousel_music/src/App.js
+++ curousel_music/src/App.js
@@ -1,23 +1,16 @@
 // App.js
 import React, { createContext, lazy, useState } from "react";
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Header from "./Components/Header";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Header } from "./Components/Header&Footer/Header";
 import Nav from "./Components/Nav/Nav";
-const HomePage = lazy(()=>import("./Components/HomePage/HomePage"));
+import { MusicProvider } from "./MusicContext";
+import { useUser } from "./UseProvider";
 import LoginPage from "./Components/Login_signup/LoginPage";
 import TrendingSongs from "./Components/TrendingSongsList/TrendingSongs";
-import { MusicProvider } from "./MusicContext";
 import TrendingSongsDetails from "./Components/TrendingSongsInHome/TrendingSongsDetails";
 import Player from "./Components/AlbumsInHome/Player";
 import SignUpPage from "./Components/Login_signup/SignUpPage";
 import MyMusic from "./Components/Sidebar/MyMusic";
-import { useUser } from "./UseProvider";
 import AlbumListUI from "./Components/AlbumList/AlbumListUI";
 import AlbumDetails from "./Components/AlbumList/AlbumDetails";
 import T20Songs from "./Components/Top20Songs/Top20Songs";
@@ -25,19 +18,18 @@ import SubscribePage from "./Components/SubscribePage/SubscribePage";
 import Radio from "./Components/Radio&Poadcast/Radio";
 import Podcast from "./Components/Radio&Poadcast/Podcast";
 
-
-
-
 export const ThemeContext = createContext(null);
 
-function App() {
-  const[theme,setTheme] = useState("dark")
+const HomePage = lazy(() => import("./Components/HomePage/HomePage"));
 
-  const toggleTheme =()=>{
-    setTheme((curr)=>(curr==="light"?"dark":"light"))
-  }
+function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   const { getToken } = useUser();
-  // console.log(getToken);
+
   function ProtectedRoute({ children }) {
     if (getToken) {
       return children;
@@ -46,26 +38,25 @@ function App() {
     }
   }
 
-
   return (
-    <ThemeContext.Provider value={{theme,toggleTheme}}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="app-container" id={theme}>
         <Router>
           <MusicProvider>
-            <Header theme={theme} toggleTheme={toggleTheme}/>
+            <Header theme={theme} toggleTheme={toggleTheme} />
             <Nav />
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/signup" element={<SignUpPage />} />
               <Route path="/TrendingSongs" element={<TrendingSongs />} />
-              <Route path="/Top_20" element ={<T20Songs/>}/>
+              <Route path="/Top_20" element={<T20Songs />} />
               <Route path="/album" element={<AlbumListUI />} />
               <Route path="/song/:id" element={<TrendingSongsDetails />} />
               <Route path="/player" element={<Player />} />
-              <Route path="/album/:id" element={<AlbumDetails/>}/>
-              <Route path="/radio" element={<Radio/>}/>
-              <Route path="/podcast" element={<Podcast/>}/>
+              <Route path="/album/:id" element={<AlbumDetails />} />
+              <Route path="/radio" element={<Radio />} />
+              <Route path="/podcast" element={<Podcast />} />
               <Route
                 path="/mymusic"
                 element={
@@ -78,7 +69,7 @@ function App() {
                 path="/subscribe"
                 element={
                   <ProtectedRoute>
-                    <SubscribePage/>
+                    <SubscribePage />
                   </ProtectedRoute>
                 }
               />
